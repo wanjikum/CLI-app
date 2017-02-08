26e71a2cf4abb282b3adee0de2d07d07ef8a072c
wanjikum <wanjikumillicent5@gmail.com>
2017-02-08 11:42:31 +0300

    Create the console application interface

---
+++ app.py
@@ -0,0 +1,103 @@
+
+#!/usr/bin/venv python
+"""
+This example uses docopt with the built in cmd module to demonstrate an
+interactive todo list command application.
+Usage:
+    app.py create_task <title>...
+    app.py get_all_tasks
+    app.py get_task_details <task_id>
+    app.py edit_task_details <task_id> <title>...
+    app.py mark_finished <task_id>
+    app.py (-i | --interactive)
+    app.py (-h | --help | --version)
+
+"""
+import os
+import sys
+import cmd
+from docopt import docopt, DocoptExit
+
+def docopt_cmd(func):
+    """
+    This decorator is used to simplify the try/except block and pass the result
+    of the docopt parsing to the called action.
+    """
+    def fn(self, arg):
+        try:
+            opt = docopt(fn.__doc__, arg)
+
+        except DocoptExit as e:
+            # The DocoptExit is thrown when the args do not match.
+            # We print a message to the user and the usage block.
+
+            print('Invalid Command!')
+            print(e)
+            return
+
+        except SystemExit:
+            # The SystemExit exception prints the usage for --help
+            # We do not need to do the print here.
+
+            return
+
+        return func(self, opt)
+
+    fn.__name__ = func.__name__
+    fn.__doc__ = func.__doc__
+    fn.__dict__.update(func.__dict__)
+    return fn
+
+def start():
+    os.system("clear")
+    intro = 'Welcome to my interactive program!'
+    prompt = 'todo>>>'
+    print(__doc__)
+
+
+
+class ToDo(cmd.Cmd):
+    intro = 'Welcome to my interactive program!'
+    prompt = 'todo>>>'
+
+    @docopt_cmd
+    def do_create_task(self, arg):
+        """Usage: app.py create_task <title>..."""
+        pass
+        
+
+    @docopt_cmd
+    def do_get_all_tasks(self, arg):
+        """Usage: get_all_tasks """
+        pass
+
+    @docopt_cmd
+    def do_get_task_details(self, arg):
+        """Usage: app.py get_task_details <task_id>"""
+        pass
+
+    @docopt_cmd
+    def do_get_edit_task_details(self, arg):
+        """Usage: app.py edit_task_details <task_id> <title>..."""
+        pass
+
+    @docopt_cmd
+    def do_mark_finished(self, arg):
+        """Usage: app.py mark_finished <task_id>"""
+        pass
+
+    def do_quit(self, arg):
+        """Quits out of Interactive Mode."""
+
+        print('Good Bye!', "cyan")
+        exit()
+
+opt = docopt(__doc__, sys.argv[1:])
+
+if __name__ == "__main__":
+    try:
+        start()
+        ToDo().cmdloop()
+    except KeyboardInterrupt:
+        os.system("clear")
+        print('Application Exiting')
